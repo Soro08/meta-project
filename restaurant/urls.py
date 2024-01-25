7f84ab5e73c779d0d8da0d389e1b7144e4323e13
Ib soro <soro4827@gmail.com>
2024-01-25 12:29:22 +0000

    add API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.index, name="home")]
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("menu/", views.MenuView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuView.as_view()),
+    path("booking/", include(router.urls)),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -4,3 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+
+# Create your views here.
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
